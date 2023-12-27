039a7017e9ec99734e48e4e8d3dbc84636af8237
joakimAndersson92 <joakim.andersson920202@gmail.com>
2023-12-27 11:44:42 +0100

    add env var

--- src/env.js
+++ src/env.js
@@ -33,7 +33,8 @@ export const env = createEnv({
     GOOGLE_CLIENT_ID: z.string(),
     GOOGLE_CLIENT_SECRET: z.string(),
     AUTH0_CLIENT_ID: z.string(),
-    AUTH0_CLIENT_SECRET: z.string()
+    AUTH0_CLIENT_SECRET: z.string(),
+    AUTH0_ISSUER: z.string()
   },
 
   /**
@@ -60,6 +61,7 @@ export const env = createEnv({
     GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
     AUTH0_CLIENT_ID: process.env.AUTH0_CLIENT_ID,
     AUTH0_CLIENT_SECRET: process.env.AUTH0_CLIENT_SECRET,
+    AUTH0_ISSUER: process.env.AUTH0_ISSUER,
   },
   /**
    * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially
